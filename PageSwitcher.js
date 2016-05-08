491d5075fed9d9aee35f4dc878c1491d25cd60a7
Kevin Ferguson <kevin@fergu.net>
2016-05-07 19:02:34 -0700

    Added nav bar transitions
    Nav bar elements now indicate which tab is active, and transitions
    between them.

--- PageSwitcher.js
+++ PageSwitcher.js
@@ -4,6 +4,11 @@ function fadeToText(id, text) {
   }).animate({'opacity': 1}, 200);
 }
 
+function selectLi(ul, liToSelect) {
+  $(ul).find('.isActive').removeClass('isActive',500).addClass('isInactive');
+  $(liToSelect).removeClass('isInactive').addClass('isActive');
+}
+
 window.onload = function() {
   // Get ID's of each of our navigation links
   var homelink = document.getElementById("homelink");
@@ -13,24 +18,29 @@ window.onload = function() {
 
   // Get ID of the main page text box
   var bodytext = document.getElementById("BodyText");
+  var navlist = document.getElementById("NavList");
 
   homelink.onclick = function() {
     fadeToText(bodytext,"Home Link Text!")
+    selectLi(navlist,homelink.parentNode);
     return false;
   }
 
   aboutlink.onclick = function() {
-    fadeToText(bodytext,"About Link Text!")
+    fadeToText(bodytext,"About Link Text!");
+    selectLi(navlist,aboutlink.parentNode);
     return false;
   }
 
   projectlink.onclick = function() {
-    fadeToText(bodytext,"Project Link Text!")
+    fadeToText(bodytext,"Project Link Text!");
+    selectLi(navlist,projectlink.parentNode);
     return false;
   }
 
   contactlink.onclick = function() {
-    fadeToText(bodytext,"Contact Link Text!")
+    fadeToText(bodytext,"Contact Link Text!");
+    selectLi(navlist,contactlink.parentNode);
     return false;
   }
 }
