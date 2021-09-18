7516cd88ceae7408fa37b7056ad0050d4e45e126
Kevin <9022843+fergu@users.noreply.github.com>
2021-09-17 17:22:38 -0700

    Cleaned up NavBar and added automatic copyright update

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ function toggle_section(dom_id) {
 	//document.getElementById(dom_id).classList.toggle('open');
 	dom_id.classList.toggle('open');
 }
+
+window.onload = function() {
+	const thisYear = (new Date()).getFullYear();
+	document.getElementById("footer").innerHTML = "Created by Kevin Ferguson, copyright 2016-"+thisYear+", all rights reserved.";
+}
