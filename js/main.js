e69ea3665e38c3fa80deacd9e5fff22a6f707050
Kevin <9022843+fergu@users.noreply.github.com>
2021-09-20 16:45:32 -0700

    Generalized the resume blocks section to be reusable on multiple pages

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ function toggle_section(dom_id) {
 }
 
 // This is the HTML template for the navigation bar.
-// Putting it here as it allows for the nav bar to be edited in a single spot
+// Putting it here as it allows for the nav bar to be edited in a single spot for all pages
+// It may be more "appropriate" to put this in its own HTML and use jquery, etc, to obtain it
 const navbar_html = `
 		<ul>
 			<li>
@@ -22,8 +23,20 @@ const navbar_html = `
 		<div id='footer'></div>
 	`
 
+
 window.onload = function() {
+	// Populate the various block section handles
+	var block_section_handle_list = document.getElementsByClassName("section-handle")
+	const section_handle_button_template = document.querySelector('#section-handle-template');
+	if (section_handle_button_template) {
+		for (const handle of block_section_handle_list) {
+			var section_handle_button = section_handle_button_template.content.cloneNode(true);
+			handle.prepend(section_handle_button);
+		}
+	}
+
 	document.getElementById("nav-bar").innerHTML = navbar_html;
+
 	const thisYear = (new Date()).getFullYear();
 	document.getElementById("footer").innerHTML = "Created by Kevin Ferguson, copyright 2016-"+thisYear+", all rights reserved.";
 }
