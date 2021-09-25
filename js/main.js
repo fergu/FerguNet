223ce706a80ec161ccab9d32404fb0bffaf324e2
Kevin <9022843+fergu@users.noreply.github.com>
2021-09-25 01:46:27 -0700

    Added page indicators and fixed another 2021 typo

--- js/main.js
+++ js/main.js
@@ -2,28 +2,6 @@ function toggle_section(dom_id) {
 	dom_id.classList.toggle('open');
 }
 
-// This is the HTML template for the navigation bar.
-// Putting it here as it allows for the nav bar to be edited in a single spot for all pages
-// It may be more "appropriate" to put this in its own HTML and use jquery, etc, to obtain it
-const navbar_html = `
-		<ul>
-			<li>
-				<a href='./index.html'>
-				<h1>./fergu.net</h1>
-				<h6>Homepage of Kevin Ferguson</h6>
-				</a>
-			</li>
-			<hr>
-			<li><a href='./resume.html'>Resume/CV</a></li>
-			<li><a href='https://www.github.com/fergu' target="_blank">Github</a></li>
-			<li><a href='./projects.html'>Projects</a></li>
-			<li><a href='./contact.html'>Contact</a></li>
-		</ul>
-		<div class='vert-spacer'></div>
-		<div id='footer'></div>
-	`
-
-
 window.onload = function() {
 	// Populate the various block section handles
 	var block_section_handle_list = document.getElementsByClassName("section-handle")
@@ -35,7 +13,7 @@ window.onload = function() {
 		}
 	}
 
-	document.getElementById("nav-bar").innerHTML = navbar_html;
+	//document.getElementById("nav-bar").innerHTML = navbar_html;
 
 	const thisYear = (new Date()).getFullYear();
 	document.getElementById("footer").innerHTML = "Created by Kevin Ferguson, copyright 2016-"+thisYear+", all rights reserved.";
