7f23c1ce9137fe92a1e179dd89dc381f81745040
Kevin <9022843+fergu@users.noreply.github.com>
2023-11-25 14:17:20 -0800

    Overhaul of website design
    This is basically a complete rework of the website design to be more modern and easier to update.

--- js/main.js
+++ js/main.js
@@ -4,17 +4,36 @@ function toggle_section(dom_id) {
 
 window.onload = function() {
 	// Populate the various block section handles
-	var block_section_handle_list = document.getElementsByClassName("section-handle")
-	const section_handle_button_template = document.querySelector('#section-handle-template');
+	var block_section_handle_list = document.getElementsByClassName("block-section-handle")
+	const section_handle_button_template = document.querySelector('#block-section-handle-template');
 	if (section_handle_button_template) {
 		for (const handle of block_section_handle_list) {
 			var section_handle_button = section_handle_button_template.content.cloneNode(true);
 			handle.prepend(section_handle_button);
 		}
 	}
+	
+	navbar = document.getElementById("nav-bar")
+	if ( navbar ) { // Only try to set the element if we found something
+		var navbar_html = ""
+		navbar_html += "<ul>"
+		navbar_html += "<li><a href='index.html'>Home</a></li>"
+		navbar_html += "<li><a href='research.html'>Research</a></li>"
+		navbar_html += "<li><a href='publications.html'>Publications</a></li>"
+		navbar_html += "<li><a href='experience.html'>Education &amp Experience</a></li>"
+		navbar_html += "<li class=\"no-hover\" style=\"flex: 1\"></li>"
+		navbar_html += "<li class=\"no-hover\"> \
+			<a href='https://www.github.com/fergu' target=\"_blank\"><img src='img/github-mark.svg' style=\"height: 24px;\" title=\"Github\" alt=\"Github\"></a> \
+			<a href='https://orcid.org/0000-0001-6653-7482' target=\"_blank\"><img src='img/ORCIDiD.svg' style=\"height: 24px\" title=\"ORCiD\" alt=\"ORCiD\"></a> \
+			<a href='mailto:subplot_spigot.0c@icloud.com?subject=fergu.net contact&body=(This email address is an iCloud relay address to avoid spambots/scrapers. I will reply from my personal email)'><img src='img/email.svg' style=\"height: 24px\" title=\"Email\" alt=\"Email me!\"></a><br> \
+		</li>"
+		navbar_html += "</ul>"
+		navbar.innerHTML = navbar_html;
+	}
 
-	//document.getElementById("nav-bar").innerHTML = navbar_html;
-
-	const thisYear = (new Date()).getFullYear();
-	document.getElementById("footer").innerHTML = "Created by Kevin Ferguson, copyright 2016-"+thisYear+", all rights reserved.";
+	footer = document.getElementById("footer")
+	if ( footer ) {
+		const thisYear = (new Date()).getFullYear();
+		footer.innerHTML = "&copy Kevin Ferguson, 2016-"+thisYear+".";
+	}
 }
